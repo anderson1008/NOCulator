7d82fd04535ca92f5118767bfea4bddce8d05384
anderson1008 <anderson.xxy@gmail.com>
2015-10-14 13:40:12 -0500

    add simulation scripts

--- hring/src/Script/sim_BLESS_synth.py
+++ hring/src/Script/sim_BLESS_synth.py
@@ -6,13 +6,13 @@ import os
 # # # # # # # # # # # # # # # # # # # # # # # # # # 16-node BLESS # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 workload_dir = "../bin/workload_list/"
-SIM_NUM = 200
-insns = "1000000"
+SIM_NUM = 500
+insns = "100000"
 
 ####################################    uniform_random    #########################################
 
 traffic = 'uniform_random' # [bit_complement, transpose, uniform_random]
-out_dir = "../results/Synthetic/" + traffic + "/BLESS/4x4/"
+out_dir = "../results/Synthetic_16B/" + traffic + "/BLESS/4x4/"
 synth_reads_fraction = 0.8
 synth_rate_base = 0.0005
 bSynthBitComplement = "false"
@@ -23,8 +23,8 @@ randomHotspot = "false"
 workload = "workloads_null"
 router_algorithm = "DR_FLIT_SWITCHED_OLDEST_FIRST"
 router_addrPacketSize = "1"
-router_dataPacketSize = "4"
-router_maxPacketSize = "4"
+router_dataPacketSize = "1"
+router_maxPacketSize = "1"
 network_nrX = "4"
 network_nrY = "4"
 topology = "Mesh"
@@ -39,90 +39,9 @@ for sim_index in range(1, SIM_NUM+1, 1):
 	command_line = "mono ../bin/sim.exe -config ../bin/config.txt -output " + out_dir + out_file + " -workload " + workload_dir + workload + ' 1' + " -router.algorithm " + router_algorithm + " -router.addrPacketSize " + router_addrPacketSize + " -router.dataPacketSize " + router_dataPacketSize + " -router.maxPacketSize " + router_maxPacketSize + " -network_nrX " + network_nrX + " -network_nrY " + network_nrY + " -topology " + topology + " -randomize_defl " + randomize_defl + " -synth_rate " + synth_rate + ' -insns ' + insns + ' -bSynthBitComplement ' + bSynthBitComplement + ' -bSynthTranspose ' + bSynthTranspose + ' -bSynthHotspot ' + bSynthHotspot + ' -randomHotspot ' + randomHotspot
 	os.system (command_line)
 
-
-####################################    bit_complement    #########################################
-
-traffic = 'bit_complement' # [bit_complement, transpose, uniform_random]
-out_dir = "../results/Synthetic/" + traffic + "/BLESS/4x4/"
-synth_reads_fraction = 0.8
-synth_rate_base = 0.0005
-bSynthBitComplement = "true"
-bSynthTranspose = "false"
-bSynthHotspot = "false"
-randomHotspot = "false"
-
-workload = "workloads_null"
-router_algorithm = "DR_FLIT_SWITCHED_OLDEST_FIRST"
-router_addrPacketSize = "1"
-router_dataPacketSize = "4"
-router_maxPacketSize = "4"
-network_nrX = "4"
-network_nrY = "4"
-topology = "Mesh"
-randomize_defl = "true"
-
-#  Injection rate sweep: 0.0005 - 0.1 at 0.0005 internal
-
-for sim_index in range(1, SIM_NUM+1, 1):
-	print ("New Simulation!")
-	synth_rate = str(synth_rate_base * sim_index)
-	out_file = "sim_" + str(sim_index) + ".out"
-	command_line = "mono ../bin/sim.exe -config ../bin/config.txt -output " + out_dir + out_file + " -workload " + workload_dir + workload + ' 1' + " -router.algorithm " + router_algorithm + " -router.addrPacketSize " + router_addrPacketSize + " -router.dataPacketSize " + router_dataPacketSize + " -router.maxPacketSize " + router_maxPacketSize + " -network_nrX " + network_nrX + " -network_nrY " + network_nrY + " -topology " + topology + " -randomize_defl " + randomize_defl + " -synth_rate " + synth_rate + ' -insns ' + insns + ' -bSynthBitComplement ' + bSynthBitComplement + ' -bSynthTranspose ' + bSynthTranspose + ' -bSynthHotspot ' + bSynthHotspot + ' -randomHotspot ' + randomHotspot
-	os.system (command_line)
-
-####################################    transpose    #########################################
-
-traffic = 'transpose' # [bit_complement, transpose, uniform_random]
-out_dir = "../results/Synthetic/" + traffic + "/BLESS/4x4/"
-synth_reads_fraction = 0.8
-synth_rate_base = 0.0005
-bSynthBitComplement = "false"
-bSynthTranspose = "true"
-bSynthHotspot = "false"
-randomHotspot = "false"
-
-workload = "workloads_null"
-router_algorithm = "DR_FLIT_SWITCHED_OLDEST_FIRST"
-router_addrPacketSize = "1"
-router_dataPacketSize = "4"
-router_maxPacketSize = "4"
-network_nrX = "4"
-network_nrY = "4"
-topology = "Mesh"
-randomize_defl = "true"
-
-#  Injection rate sweep: 0.0005 - 0.1 at 0.0005 internal
-
-for sim_index in range(1, SIM_NUM+1, 1):
-	print ("New Simulation!")
-	synth_rate = str(synth_rate_base * sim_index)
-	out_file = "sim_" + str(sim_index) + ".out"
-	command_line = "mono ../bin/sim.exe -config ../bin/config.txt -output " + out_dir + out_file + " -workload " + workload_dir + workload + ' 1' + " -router.algorithm " + router_algorithm + " -router.addrPacketSize " + router_addrPacketSize + " -router.dataPacketSize " + router_dataPacketSize + " -router.maxPacketSize " + router_maxPacketSize + " -network_nrX " + network_nrX + " -network_nrY " + network_nrY + " -topology " + topology + " -randomize_defl " + randomize_defl + " -synth_rate " + synth_rate + ' -insns ' + insns + ' -bSynthBitComplement ' + bSynthBitComplement + ' -bSynthTranspose ' + bSynthTranspose + ' -bSynthHotspot ' + bSynthHotspot + ' -randomHotspot ' + randomHotspot
-	os.system (command_line)
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # 64-node BLESS # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-traffic = 'uniform_random' # [bit_complement, transpose, uniform_random]
-out_dir = "../results/Synthetic/" + traffic + "/BLESS/8x8/"
-synth_reads_fraction = 0.8
-synth_rate_base = 0.0005
-bSynthBitComplement = "false"
-bSynthTranspose = "false"
-bSynthHotspot = "false"
-randomHotspot = "false"
-
-workload = "workloads_null"
-router_algorithm = "DR_FLIT_SWITCHED_OLDEST_FIRST"
-router_addrPacketSize = "1"
-router_dataPacketSize = "4"
-router_maxPacketSize = "4"
+out_dir = "../results/Synthetic_16B/" + traffic + "/BLESS/8x8/"
 network_nrX = "8"
 network_nrY = "8"
-topology = "Mesh"
-randomize_defl = "true"
-
-#  Injection rate sweep: 0.0005 - 0.1 at 0.0005 internal
-
 for sim_index in range(1, SIM_NUM+1, 1):
 	print ("New Simulation!")
 	synth_rate = str(synth_rate_base * sim_index)
@@ -130,158 +49,14 @@ for sim_index in range(1, SIM_NUM+1, 1):
 	command_line = "mono ../bin/sim.exe -config ../bin/config.txt -output " + out_dir + out_file + " -workload " + workload_dir + workload + ' 1' + " -router.algorithm " + router_algorithm + " -router.addrPacketSize " + router_addrPacketSize + " -router.dataPacketSize " + router_dataPacketSize + " -router.maxPacketSize " + router_maxPacketSize + " -network_nrX " + network_nrX + " -network_nrY " + network_nrY + " -topology " + topology + " -randomize_defl " + randomize_defl + " -synth_rate " + synth_rate + ' -insns ' + insns + ' -bSynthBitComplement ' + bSynthBitComplement + ' -bSynthTranspose ' + bSynthTranspose + ' -bSynthHotspot ' + bSynthHotspot + ' -randomHotspot ' + randomHotspot
 	os.system (command_line)
 
-
-####################################    bit_complement    #########################################
-
-traffic = 'bit_complement' # [bit_complement, transpose, uniform_random]
-out_dir = "../results/Synthetic/" + traffic + "/BLESS/8x8/"
-synth_reads_fraction = 0.8
-synth_rate_base = 0.0005
-bSynthBitComplement = "true"
-bSynthTranspose = "false"
-bSynthHotspot = "false"
-randomHotspot = "false"
-
-workload = "workloads_null"
-router_algorithm = "DR_FLIT_SWITCHED_OLDEST_FIRST"
-router_addrPacketSize = "1"
-router_dataPacketSize = "4"
-router_maxPacketSize = "4"
-network_nrX = "8"
-network_nrY = "8"
-topology = "Mesh"
-randomize_defl = "true"
-
-#  Injection rate sweep: 0.0005 - 0.1 at 0.0005 internal
-
-for sim_index in range(1, SIM_NUM+1, 1):
-	print ("New Simulation!")
-	synth_rate = str(synth_rate_base * sim_index)
-	out_file = "sim_" + str(sim_index) + ".out"
-	command_line = "mono ../bin/sim.exe -config ../bin/config.txt -output " + out_dir + out_file + " -workload " + workload_dir + workload + ' 1' + " -router.algorithm " + router_algorithm + " -router.addrPacketSize " + router_addrPacketSize + " -router.dataPacketSize " + router_dataPacketSize + " -router.maxPacketSize " + router_maxPacketSize + " -network_nrX " + network_nrX + " -network_nrY " + network_nrY + " -topology " + topology + " -randomize_defl " + randomize_defl + " -synth_rate " + synth_rate + ' -insns ' + insns + ' -bSynthBitComplement ' + bSynthBitComplement + ' -bSynthTranspose ' + bSynthTranspose + ' -bSynthHotspot ' + bSynthHotspot + ' -randomHotspot ' + randomHotspot
-	os.system (command_line)
-
-####################################    transpose    #########################################
-
-traffic = 'transpose' # [bit_complement, transpose, uniform_random]
-out_dir = "../results/Synthetic/" + traffic + "/BLESS/8x8/"
-synth_reads_fraction = 0.8
-synth_rate_base = 0.0005
-bSynthBitComplement = "false"
-bSynthTranspose = "true"
-bSynthHotspot = "false"
-randomHotspot = "false"
-
-workload = "workloads_null"
-router_algorithm = "DR_FLIT_SWITCHED_OLDEST_FIRST"
-router_addrPacketSize = "1"
-router_dataPacketSize = "4"
-router_maxPacketSize = "4"
-network_nrX = "8"
-network_nrY = "8"
-topology = "Mesh"
-randomize_defl = "true"
-
-#  Injection rate sweep: 0.0005 - 0.1 at 0.0005 internal
-
-for sim_index in range(1, SIM_NUM+1, 1):
-	print ("New Simulation!")
-	synth_rate = str(synth_rate_base * sim_index)
-	out_file = "sim_" + str(sim_index) + ".out"
-	command_line = "mono ../bin/sim.exe -config ../bin/config.txt -output " + out_dir + out_file + " -workload " + workload_dir + workload + ' 1' + " -router.algorithm " + router_algorithm + " -router.addrPacketSize " + router_addrPacketSize + " -router.dataPacketSize " + router_dataPacketSize + " -router.maxPacketSize " + router_maxPacketSize + " -network_nrX " + network_nrX + " -network_nrY " + network_nrY + " -topology " + topology + " -randomize_defl " + randomize_defl + " -synth_rate " + synth_rate + ' -insns ' + insns + ' -bSynthBitComplement ' + bSynthBitComplement + ' -bSynthTranspose ' + bSynthTranspose + ' -bSynthHotspot ' + bSynthHotspot + ' -randomHotspot ' + randomHotspot
-	os.system (command_line)
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # 256-node BLESS # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-traffic = 'uniform_random' # [bit_complement, transpose, uniform_random]
-out_dir = "../results/Synthetic/" + traffic + "/BLESS/16x16/"
-synth_reads_fraction = 0.8
-synth_rate_base = 0.0005
-bSynthBitComplement = "false"
-bSynthTranspose = "false"
-bSynthHotspot = "false"
-randomHotspot = "false"
-
-workload = "workloads_null"
-router_algorithm = "DR_FLIT_SWITCHED_OLDEST_FIRST"
-router_addrPacketSize = "1"
-router_dataPacketSize = "4"
-router_maxPacketSize = "4"
+out_dir = "../results/Synthetic_16B/" + traffic + "/BLESS/16x16/"
 network_nrX = "16"
 network_nrY = "16"
-topology = "Mesh"
-randomize_defl = "true"
-
-#  Injection rate sweep: 0.0005 - 0.1 at 0.0005 internal
-
 for sim_index in range(1, SIM_NUM+1, 1):
 	print ("New Simulation!")
 	synth_rate = str(synth_rate_base * sim_index)
 	out_file = "sim_" + str(sim_index) + ".out"
 	command_line = "mono ../bin/sim.exe -config ../bin/config.txt -output " + out_dir + out_file + " -workload " + workload_dir + workload + ' 1' + " -router.algorithm " + router_algorithm + " -router.addrPacketSize " + router_addrPacketSize + " -router.dataPacketSize " + router_dataPacketSize + " -router.maxPacketSize " + router_maxPacketSize + " -network_nrX " + network_nrX + " -network_nrY " + network_nrY + " -topology " + topology + " -randomize_defl " + randomize_defl + " -synth_rate " + synth_rate + ' -insns ' + insns + ' -bSynthBitComplement ' + bSynthBitComplement + ' -bSynthTranspose ' + bSynthTranspose + ' -bSynthHotspot ' + bSynthHotspot + ' -randomHotspot ' + randomHotspot
 	os.system (command_line)
-
-
-####################################    bit_complement    #########################################
-
-traffic = 'bit_complement' # [bit_complement, transpose, uniform_random]
-out_dir = "../results/Synthetic/" + traffic + "/BLESS/16x16/"
-synth_reads_fraction = 0.8
-synth_rate_base = 0.0005
-bSynthBitComplement = "true"
-bSynthTranspose = "false"
-bSynthHotspot = "false"
-randomHotspot = "false"
-
-workload = "workloads_null"
-router_algorithm = "DR_FLIT_SWITCHED_OLDEST_FIRST"
-router_addrPacketSize = "1"
-router_dataPacketSize = "4"
-router_maxPacketSize = "4"
-network_nrX = "16"
-network_nrY = "16"
-topology = "Mesh"
-randomize_defl = "true"
-
-#  Injection rate sweep: 0.0005 - 0.1 at 0.0005 internal
-
-for sim_index in range(1, SIM_NUM+1, 1):
-	print ("New Simulation!")
-	synth_rate = str(synth_rate_base * sim_index)
-	out_file = "sim_" + str(sim_index) + ".out"
-	command_line = "mono ../bin/sim.exe -config ../bin/config.txt -output " + out_dir + out_file + " -workload " + workload_dir + workload + ' 1' + " -router.algorithm " + router_algorithm + " -router.addrPacketSize " + router_addrPacketSize + " -router.dataPacketSize " + router_dataPacketSize + " -router.maxPacketSize " + router_maxPacketSize + " -network_nrX " + network_nrX + " -network_nrY " + network_nrY + " -topology " + topology + " -randomize_defl " + randomize_defl + " -synth_rate " + synth_rate + ' -insns ' + insns + ' -bSynthBitComplement ' + bSynthBitComplement + ' -bSynthTranspose ' + bSynthTranspose + ' -bSynthHotspot ' + bSynthHotspot + ' -randomHotspot ' + randomHotspot
-	os.system (command_line)
-
-####################################    transpose    #########################################
-
-traffic = 'transpose' # [bit_complement, transpose, uniform_random]
-out_dir = "../results/Synthetic/" + traffic + "/BLESS/16x16/"
-synth_reads_fraction = 0.8
-synth_rate_base = 0.0005
-bSynthBitComplement = "false"
-bSynthTranspose = "true"
-bSynthHotspot = "false"
-randomHotspot = "false"
-
-workload = "workloads_null"
-router_algorithm = "DR_FLIT_SWITCHED_OLDEST_FIRST"
-router_addrPacketSize = "1"
-router_dataPacketSize = "4"
-router_maxPacketSize = "4"
-network_nrX = "16"
-network_nrY = "16"
-topology = "Mesh"
-randomize_defl = "true"
-
-#  Injection rate sweep: 0.0005 - 0.1 at 0.0005 internal
-
-for sim_index in range(1, SIM_NUM+1, 1):
-	print ("New Simulation!")
-	synth_rate = str(synth_rate_base * sim_index)
-	out_file = "sim_" + str(sim_index) + ".out"
-	command_line = "mono ../bin/sim.exe -config ../bin/config.txt -output " + out_dir + out_file + " -workload " + workload_dir + workload + ' 1' + " -router.algorithm " + router_algorithm + " -router.addrPacketSize " + router_addrPacketSize + " -router.dataPacketSize " + router_dataPacketSize + " -router.maxPacketSize " + router_maxPacketSize + " -network_nrX " + network_nrX + " -network_nrY " + network_nrY + " -topology " + topology + " -randomize_defl " + randomize_defl + " -synth_rate " + synth_rate + ' -insns ' + insns + ' -bSynthBitComplement ' + bSynthBitComplement + ' -bSynthTranspose ' + bSynthTranspose + ' -bSynthHotspot ' + bSynthHotspot + ' -randomHotspot ' + randomHotspot
-	os.system (command_line)
-
 
 
--- hring/src/Script/sim_MBNoC_synth_256b.py
+++ hring/src/Script/sim_MBNoC_synth_256b.py
@@ -4,8 +4,8 @@ import sys
 import os
 
 workload_dir = "../bin/workload_list/"
-SIM_NUM = 500
-insns = "100000"
+SIM_NUM = 200
+insns = "1000000"
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # 16-node BLESS # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -13,7 +13,7 @@ insns = "100000"
 
 
 traffic = 'uniform_random' # [bit_complement, transpose, uniform_random]
-out_dir = "../results/Synthetic_16B/" + traffic + "/MBNoC4/4x4/"
+out_dir = "../results/Synthetic/" + traffic + "/MBNoC_256b/4x4/"
 synth_reads_fraction = 0.8
 synth_rate_base = 0.0005
 bSynthBitComplement = "false"
@@ -23,13 +23,13 @@ randomHotspot = "false"
 
 workload = "workloads_null"
 router_algorithm = "BLESS_BYPASS"
-router_addrPacketSize = "4"
+router_addrPacketSize = "1"
 router_dataPacketSize = "4"
 router_maxPacketSize = "4"
-network_nrX = "4"
-network_nrY = "4"
+network_nrX = "16"
+network_nrY = "16"
 topology = "Mesh_Multi"
-sub_net = "4"
+sub_net = "2"
 subnet_sel_rand = "false"
 
 #  Injection rate sweep: 0.0005 - 0.1 at 0.0005 internal
@@ -39,16 +39,5 @@ for sim_index in range(1, SIM_NUM+1, 1):
 	synth_rate = str(synth_rate_base * sim_index)
 	out_file = "sim_" + str(sim_index) + ".out"
 	command_line = "mono ../bin/sim.exe -config ../bin/config.txt -output " + out_dir + out_file + " -workload " + workload_dir + workload + ' 1' + " -router.algorithm " + router_algorithm + " -router.addrPacketSize " + router_addrPacketSize + " -router.dataPacketSize " + router_dataPacketSize + " -router.maxPacketSize " + router_maxPacketSize + " -network_nrX " + network_nrX + " -network_nrY " + network_nrY + " -topology " + topology + " -synth_rate " + synth_rate + ' -insns ' + insns + ' -bSynthBitComplement ' + bSynthBitComplement + ' -bSynthTranspose ' + bSynthTranspose + ' -bSynthHotspot ' + bSynthHotspot + ' -randomHotspot ' + randomHotspot + ' -sub_net ' + sub_net + ' -subnet_sel_rand ' + subnet_sel_rand
-	os.system (command_line)
-
-out_dir = "../results/Synthetic_16B/" + traffic + "/MBNoC4/8x8/"
-network_nrX = "8"
-network_nrY = "8"
-
-for sim_index in range(1, SIM_NUM+1, 1):
-	print ("New Simulation!")
-	synth_rate = str(synth_rate_base * sim_index)
-	out_file = "sim_" + str(sim_index) + ".out"
-	command_line = "mono ../bin/sim.exe -config ../bin/config.txt -output " +  out_dir + out_file + " -workload " + workload_dir + workload + ' 1' + " -router.algorithm " + router_algorithm + " -router.addrPacketSize " + router_addrPacketSize + " -router.dataPacketSize " + router_dataPacketSize + " -router.maxPacketSize " + router_maxPacketSize + " -network_nrX " +  network_nrX + " -network_nrY " + network_nrY + " -topology " + topology + " -synth_rate " + synth_rate + ' -insns ' + insns + ' -bSynthBitComplement ' + bSynthBitComplement + ' -bSynthTranspose ' + bSynthTranspose + ' -bSynthHotspot ' + bSynthHotspot + ' -randomHotspot ' + randomHotspot + ' -sub_net ' + sub_net + ' -subnet_sel_rand ' + subnet_sel_rand
 	os.system (command_line)
 
